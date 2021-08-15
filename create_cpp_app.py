b0283553c7c4df0914ad5ab36a3942152ab8fb32
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2021-08-15 09:22:22 -0400

    remove valgrind

--- create_cpp_app.py
+++ create_cpp_app.py
@@ -14,57 +14,7 @@ import utility_data
         check   = run c++ tests
 """
 
-"""
-def get_valgrind_result(executable_path):
-    os.chdir(os.path.join("include", "valgrind"))
-    subprocess.call(["valgrind", "--log-file={}".format("output.txt"), executable_path])
-    os.chdir(os.path.join("..", ".."))
-    #os.system("rm -r app.dSYM")
-
-def parse_valgrind_info():
-    with open(os.path.join("include", "valgrind", "output.txt"), "r") as fout:
-        return "All heap blocks were freed -- no leaks are possible" in fout.read()
-"""
-def build():
-    os.system("make build")
-    #get_valgrind_result(os.path.join("..","..","app"))
-    #print(parse_valgrind_info())
-
-def check():
-    os.system("make check")
-
-def run():
-    os.system("make run")
-
 ######################################################################
-
-def create_valgrind_output_dir(project_name):
-    os.mkdir(os.path.join(os.getcwd(), project_name, "include", "valgrinddata"))
-    with open(os.path.join(os.getcwd(), project_name, "include", "valgrinddata", "output.txt"), "w+") as f:
-        pass
-
-def install_valgrind_files(project_name):
-    if (sys.platform == "darwin" or sys.platform == "os2" or sys.platform == "os2emx"):
-        url = "https://github.com/LouisBrunner/valgrind-macos.git"
-    else:
-        url = "https://github.com/tklengyel/valgrind.git"
-    os.chdir(os.path.join(os.getcwd(), project_name, "include"))
-    os.system("git clone '{}'".format(url))
-    # finish installing valgrind
-    try:
-        subprocess.call(["valgrind"])
-        os.chdir("../..")
-    except FileNotFoundError:
-        os.chdir("valgrind-macos")
-        os.system("./autogen.sh")
-        os.system("./configure")
-        os.system("make")
-        if sys.platform == "win32":
-            os.system("make install")
-        else:
-            os.system("sudo make install")
-        os.system("valgrind") # check that it was installed
-        os.chdir("../../..")
 
 def fetch_catch_file():
     r = requests.get("https://github.com/philsquared/Catch/releases/download/v1.12.2/catch.hpp", allow_redirects=True)
@@ -164,17 +114,6 @@ if __name__ == "__main__":
             sys.exit(-1)
 
         #########################################
-        #   INSTALL THE VALGRIND FILES          #
-        #########################################
-        try:
-            install_valgrind_files(project_name)
-            create_valgrind_output_dir(project_name)
-            print(colorama.Back.GREEN + "[+] Successfully installed the valgrind dependency files")
-        except Exception as err:
-            print(colorama.Back.RED + "[-] Error: Failed to create the valgrind dependency file. {}".format(str(err).split(":")[0]))
-            sys.exit(-1)
-
-        #########################################
         #   CREATE THE MAKEfile                 #
         #########################################
         try:
@@ -240,21 +179,6 @@ if __name__ == "__main__":
             sys.exit(-1)
     elif (command.lower() == "--help"):
         print("TODO - add help menu")
-    elif (command.lower() == "build"):
-        try:
-            build()
-        except Exception as err:
-            sys.exit(-1)
-    elif (command.lower() == "check"):
-        try:
-            check()
-        except Exception as err:
-            sys.exit(-1)
-    elif (command.lower() == "run"):
-        try:
-            run()
-        except Exception as err:
-            sys.exit(-1)
     else:
         print(colorama.Back.RED + "[-] Error: Command '{}' is not valid. Try --help to get a full list of commands".format(command))
         sys.exit(-1)
